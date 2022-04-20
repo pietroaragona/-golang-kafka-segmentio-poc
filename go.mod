9bf5ccd7bfc4c653c7e64619693065642d0ba6b6
paragona <paragona@sogei.it>
2022-04-20 14:11:25 +0000

    add kafka producer/consumer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pietroaragona/golang-kafka-segmentio-poc
 
 go 1.16
+
+require github.com/segmentio/kafka-go v0.4.31 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/segmentio/kafka-go"
+	"time"
+)
+
+const (
+	topic     = "foo"
+	partition = 0
+)
 
 func main() {
 	fmt.Println("Hello from golang")
+
+	produce()
+	consume()
+
+}
+
+func produce() {
+	fmt.Println("START Produce")
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "kafka-svc:9092", topic, partition)
+	if err != nil {
+		fmt.Println("failed to dial leader:", err)
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: []byte("one!")},
+		kafka.Message{Value: []byte("two!")},
+		kafka.Message{Value: []byte("three!")},
+	)
+	if err != nil {
+		fmt.Println("failed to write messages:", err)
+	}
+	if err := conn.Close(); err != nil {
+		fmt.Println("failed to close writer:", err)
+	}
+	fmt.Println("Conn Close")
+
+}
+
+func consume() {
+	fmt.Println("START Consume")
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "kafka-svc:9092", topic, partition)
+	if err != nil {
+		fmt.Println("failed to dial leader:", err)
+	}
+
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	batch := conn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
+	//b := make([]byte, 10e3)            // 10KB max per message
+	for {
+		b := make([]byte, 10e3)
+		_, err := batch.Read(b)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		fmt.Println(string(b))
+	}
+
+	if err := batch.Close(); err != nil {
+		fmt.Println("failed to close batch:", err)
+	}
+
 }
